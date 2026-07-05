b20af6b4ac95f375e74ca033e9926aac7ca961e7
Vaibhav Dwivedi <vibhu210703@gmail.com>
2026-07-05 23:56:41 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://https://smart-campus-5afh.onrender.com/api'
+  baseURL: 'https://smart-campus-5afh.onrender.com/api'
 });
 
 API.interceptors.request.use((req) => {
